b520e692252055eac52f3313480b61a4841e15ce
Mac <nmacawile@gmail.com>
2019-01-19 12:42:45 +0000

    Add custom error classes

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,32 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
   
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+  
   included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
-    end
-    
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_handler
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_handler
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_handler
+    rescue_from ExceptionHandler::MissingToken, with: :unprocessable_handler
+    rescue_from ExceptionHandler::InvalidToken, with: :unprocessable_handler
+  end
+  
+  private
+  
+  # 422 error
+  def unprocessable_handler(e)
+    json_response({ message: e.message }, :unprocessable_entity)
+  end
+  
+  # 401 error
+  def unauthorized_handler(e)
+    json_response({ message: e.message }, :unauthorized)
+  end
+  
+  # 404 error
+  def not_found_handler(e)
+    json_response({ message: e.message }, :not_found)
   end
 end
