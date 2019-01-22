f7b0151ece77dc53dc2e4169c56517f70757e844
Mac <nmacawile@gmail.com>
2019-01-22 16:41:57 +0000

    Fix spec helper typo

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -9,7 +9,7 @@ module ControllerSpecHelper
   
   def valid_headers
     {
-      "Authorization": token_generator(user_id),
+      "Authorization": token_generator(user.id),
       "Content-Type": "application/json"
     }
   end
